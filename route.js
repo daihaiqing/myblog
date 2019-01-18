80019cf97fe9affd4142c8601f465cad6a0b3841
haiqing.dai <haiqing.dai@renren-inc.com>
2019-01-18 14:48:31 +0800

    测试ensure

--- route.js
+++ route.js
@@ -1,28 +1,36 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import homePage from './src/views/home.vue';
-import aboutPage from './src/views/about.vue';
-import start from './src/views/start.vue';
-import frontFaq from './src/views/FrontToEnd-faq.vue'
-Vue.use(Router)
+// import homePage from './src/views/home.vue';
+// import aboutPage from './src/views/about.vue';
+// import start from './src/views/start.vue';
+// import frontFaq from './src/views/FrontToEnd-faq.vue';
 
+Vue.use(Router);
 export default new Router({
     routes:[
         {
             path:'/',
-            component:homePage
+            component:require.ensure([], (require) => {
+                require('./src/views/home.vue');
+            })
         },
         {
             path:'/about',
-            component:aboutPage
+            component:require.ensure([], (require) => {
+                require('./src/views/about.vue');
+            })
         },
         {
-            path:'frontFaq',
-            component:frontFaq
+            path:'/frontFaq',
+            component:require.ensure([], (require) => {
+                require('./src/views/FrontToEnd-faq.vue');
+            })
         },
         {
             path:'/start',
-            component:start
+            component:require.ensure([], (require) => {
+                require('./src/views/start.vue');
+            })
         }
     ]
-})
+});

aba7b652062fc97bbed94239791a0c77f106e576
haiqing.dai <haiqing.dai@renren-inc.com>
2019-02-22 14:23:06 +0800

    fix(module) : notification 文案

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,3 +1,39 @@
 <template>
-    <div>about</div>
+    <div>
+        about
+    </div>
 </template>
+
+<script>
+var recorder;
+export default {
+    name: 'test',
+    data () {
+        return {
+        }
+    },
+    methods:{
+        startRecording(data) {
+            console.error('start');
+            if(this.stopRecording(data) === 'return'){
+                return;
+            }
+            console.error('end');
+        },
+        stopRecording(data) {
+            console.error('stop');
+            if(data !== 'return'){
+                return ;
+            }else{
+                return data;
+            }
+            // console.error(data);
+        },
+    },
+    mounted(){
+        this.startRecording('return');
+        console.error('================================');
+        this.startRecording('12');
+    }
+}
+</script>
